329db002b3902a956247e4f9c9ddc363197143ed
marijavelkovica <manya_v@inbox.lv>
2022-05-15 19:11:58 +0300

    Fixed id generation, added db url to config file

--- src/main/java/com/example/demo/entity/Appointment.java
+++ src/main/java/com/example/demo/entity/Appointment.java
@@ -4,10 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Entity(name = "APPOINTMENT")
 public class Appointment {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
